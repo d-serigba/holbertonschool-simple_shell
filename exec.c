ceb741b4b04489b469f97e4adc35c4db09b41b63
Niber236 <berniskouzou45@gmail.com>
2026-01-05 13:37:56 +0000

    Fix error message to Permission denied

--- exec.c
+++ exec.c
@@ -7,20 +7,24 @@
 */
 int new_process(char **args, char *prog_name)
 {
-pid_t pid;
-int status;
-pid = fork();
-if (pid == 0)
-{
-if (execve(args[0], args, environ) == -1)
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-perror("fork");
-else
-wait(&status);
-return (1);
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			/* Si on est ici, le fichier existe mais ne peut pas etre execute */
+			fprintf(stderr, "%s: 1: %s: Permission denied\n", prog_name, args[0]);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+		perror("fork");
+	else
+		wait(&status);
+	return (1);
 }
 /**
 * execute_args - Wrapper
@@ -30,5 +34,5 @@ return (1);
 */
 int execute_args(char **args, char *prog_name)
 {
-return (new_process(args, prog_name));
+	return (new_process(args, prog_name));
 }
